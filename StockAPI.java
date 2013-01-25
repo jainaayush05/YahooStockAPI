ec9cf54b6d4d3f708f18058cd40a6e2b6633ab6e
jainaayush05 <jainaayush05@gmail.com>
2013-01-26 03:07:48 +0530

    Update StockAPI.java

--- StockAPI.java
+++ StockAPI.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class StockAPI {
-  private static HashMap<String, StockModel> stocks = new HashMap<String, StockModel>();
+	private static HashMap<String, StockModel> stocks = new HashMap<String, StockModel>();
 	
 	public static StockModel getStockData(String symbol) {
 		try {
@@ -25,17 +25,17 @@ public class StockAPI {
 				StockModel stockInfo = new StockModel();
 				stockInfo.setTicker(yahooStockInfo[0].replaceAll("\"", ""));
 				stockInfo.setName(yahooStockInfo[1].replaceAll("\"", ""));
-				stockInfo.setPrice(Float.valueOf(yahooStockInfo[2]));
-				stockInfo.setChange(yahooStockInfo[3]);
-				stockInfo.setDayLow(Float.valueOf(yahooStockInfo[4]));
-				stockInfo.setDayHigh(Float.valueOf(yahooStockInfo[5]));
-				stockInfo.setYearLow(Float.valueOf(yahooStockInfo[6]));
-				stockInfo.setYearHigh(Float.valueOf(yahooStockInfo[7]));
-				stockInfo.setMarketCap(yahooStockInfo[8]);
-				stockInfo.setOpen(Float.valueOf(yahooStockInfo[9]));
-				stockInfo.setPrevClose(Float.valueOf(yahooStockInfo[10]));
-				stockInfo.setChangePercent(yahooStockInfo[11]);
-				stockInfo.setVolume(Long.valueOf(yahooStockInfo[12]));
+				stockInfo.setPrice(yahooStockInfo[2].replaceAll("\"", ""));
+				stockInfo.setChange(yahooStockInfo[3].replaceAll("\"", ""));
+				stockInfo.setDayLow(yahooStockInfo[4].replaceAll("\"", ""));
+				stockInfo.setDayHigh(yahooStockInfo[5].replaceAll("\"", ""));
+				stockInfo.setYearLow(yahooStockInfo[6].replaceAll("\"", ""));
+				stockInfo.setYearHigh(yahooStockInfo[7].replaceAll("\"", ""));
+				stockInfo.setMarketCap(yahooStockInfo[8].replaceAll("\"", ""));
+				stockInfo.setOpen(yahooStockInfo[9].replaceAll("\"", ""));
+				stockInfo.setPrevClose(yahooStockInfo[10].replaceAll("\"", ""));
+				stockInfo.setChangePercent(yahooStockInfo[11].replaceAll("\"", ""));
+				stockInfo.setVolume(yahooStockInfo[12].replaceAll("\"", ""));
 				stockInfo.setChartUrlSmall("http://ichart.finance.yahoo.com/t?s=" + stockInfo.getTicker());
 				stockInfo.setChartUrlLarge("http://chart.finance.yahoo.com/w?s=" + stockInfo.getTicker());
 				stockInfo.setLastUpdated((new Date()).getTime());
